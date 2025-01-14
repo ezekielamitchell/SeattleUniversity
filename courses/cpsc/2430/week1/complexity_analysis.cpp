d701b7dbd165c6ea21f353d5ee7e5c2e86144bda
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-01-14 12:22:20 -0800

    cpsc 2430 init

---
+++ courses/cpsc/2430/week1/complexity_analysis.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+void numberSwapOne(int x, int y) {
+    auto start = high_resolution_clock::now();
+
+    int temp = x;
+    x = y;
+    y = temp;
+
+
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Method 1: " << duration.count() << endl;
+
+}
+
+void numberSwapTwo(int x, int y) {
+
+    auto start = high_resolution_clock::now(); // start timer
+
+    x = x + y;
+    y = x - y;
+    x = x - y;
+
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+
+    auto stop = high_resolution_clock::now(); // end timer
+    auto duration = duration_cast<microseconds>(stop - start);
+    
+    cout << "Method 2: " << duration.count() << endl;
+
+}
+
+int main() {
+    numberSwapOne(5, 4);
+    numberSwapTwo(5, 4);
+}
