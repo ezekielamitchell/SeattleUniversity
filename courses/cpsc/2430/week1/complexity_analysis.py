47b507b83cde4f0ddee2c4652a65bf0b35ec5cc9
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-01-21 14:31:25 -0800

    React + FastAPI

--- courses/cpsc/2430/week1/complexity_analysis.py
+++ courses/cpsc/2430/week1/complexity_analysis.py
@@ -1,1 +1,13 @@
-print(1)
+
+
+# time comparison
+
+def numberSwapOne(a,b):
+    temp = a
+    a = b
+    b = temp
+    print(f'int: a: {a}, b: {b}')
+
+# main function
+if __name__ == '__main__':
+    numberSwapOne(10, 50)
