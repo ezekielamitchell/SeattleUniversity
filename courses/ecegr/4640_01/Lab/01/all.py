1d7469ed01c752fd4b4114746ac4fd48f9859bd9
Ezekiel A. Mitchell <Ezekielam@icloud.com>
2025-10-10 20:07:11 -0700

    IoT init + Lab 1

---
+++ courses/ecegr/4640_01/Lab/01/all.py
@@ -0,0 +1,41 @@
+from time import time, sleep
+from urllib.request import urlopen
+import urllib.error
+import sys
+from envirophat import motion, weather, light, analog
+import math
+
+WRITE_API = 'YZS13Z5HLRLHSLP7'
+BASE_URL = "https://api.thingspeak.com/update?api_key={}".format(WRITE_API)
+
+SensorPrevSec = 0
+SensorInterval = 2 # 2 seconds
+ThingSpeakPrevSec = 0
+ThingSpeakInterval = 20 # 20 seconds
+
+# Initialize variables
+sun = 0
+logg = [0, 0, 0, 0]
+temperature = 0
+x, y, z = 0, 0, 0
+
+try:
+    while True:
+        if time() - SensorPrevSec > SensorInterval:
+            SensorPrevSec = time()
+
+            sun = light.light()
+            logg = analog.read_all()
+            temperature = weather.temperature()
+            x, y, z = motion.accelerometer()
+
+        if time() - ThingSpeakPrevSec > ThingSpeakInterval:
+            ThingSpeakPrevSec = time()
+            thingspeakHttp = BASE_URL + "&field1={:.2f}&field2={:.2f}&field3={:.2f}&field4={:.2f}".format(temperature, sun, math.sqrt(x**2 + y**2 + z**2), logg[0])
+            print(thingspeakHttp)
+            conn = urlopen(thingspeakHttp)
+            print('Response: {}', format(conn.read()))
+            conn.close()
+            sleep(1)
+except KeyboardInterrupt:
+    conn.close()
---
+++ courses/ecegr/4640_01/Lab/01/temperature.py
@@ -0,0 +1,34 @@
+from time import time, sleep
+from urllib.request import urlopen
+import urllib.error
+import sys
+from envirophat import weather, leds
+
+WRITE_API = "TJ2D3LRDO4A4BEVO" # Replace your ThingSpeak API key here
+BASE_URL ="https://api.thingspeak.com/update?api_key={}".format(WRITE_API)
+
+SensorPrevSec = 0
+SensorInterval = 2 # 2 seconds
+ThingSpeakPrevSec = 0
+ThingSpeakInterval = 20 # 20 seconds
+temperature = weather.temperature()
+
+
+try:
+    while True:
+        if time() - SensorPrevSec > SensorInterval:
+            SensorPrevSec = time()
+
+            temperature = weather.temperature()
+            print("{} degrees Celcius".format(temperature))
+
+        if time() - ThingSpeakPrevSec > ThingSpeakInterval:
+            ThingSpeakPrevSec = time()
+            thingspeakHttp = BASE_URL + "&field1={:.2f}".format(temperature)
+            print(thingspeakHttp)
+            conn = urlopen(thingspeakHttp)
+            print("Response: {}".format(conn.read()))
+            conn.close()
+            sleep(1)
+except KeyboardInterrupt:
+    conn.close()
